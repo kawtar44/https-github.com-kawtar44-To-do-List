2568dead1b6dd5b1336cf92ac9ab8c410d842edb
redao <redaovish200@hotmail.com>
2020-06-22 23:19:24 +0100

    to do list - tp2 - questions 1-7

---
+++ app/src/main/java/com/example/to_do_list/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.to_do_list;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> todoItems;
+    private EditText mavariableEditText;
+    private ListView mavariableListView;
+    private ArrayAdapter<String> aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        mavariableEditText=findViewById(R.id.editText);
+        mavariableListView=findViewById(R.id.list);
+        todoItems = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,todoItems);
+        mavariableListView.setAdapter(aa);
+
+        mavariableEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction()== KeyEvent.ACTION_DOWN)
+                    if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+                {
+                    todoItems.add(mavariableEditText.getText().toString());
+                    aa.notifyDataSetChanged();
+                    mavariableEditText.setText("");
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
